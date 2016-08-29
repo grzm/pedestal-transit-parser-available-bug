4babcbcb02d2c0d6e1086791527cd4fc01989c1f
Michael Glaesemann <grzm@seespotcode.net>
2016-08-28 21:20:52 -0400

    Require pedestal 0.5.0
    Change-Id: I2a092883ea66c62cb14ead15275e09d720f2fca3

--- project.clj
+++ project.clj
@@ -4,11 +4,11 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [io.pedestal/pedestal.service "0.5.1"]
+                 [io.pedestal/pedestal.service "0.5.0"]
 
                  ;; Remove this line and uncomment one of the next lines to
                  ;; use Immutant or Tomcat instead of Jetty:
-                 [io.pedestal/pedestal.jetty "0.5.1"]
+                 [io.pedestal/pedestal.jetty "0.5.0"]
                  ;; [io.pedestal/pedestal.immutant "0.5.1"]
                  ;; [io.pedestal/pedestal.tomcat "0.5.1"]
 
